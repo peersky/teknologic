650e91827618387e40bc202469af7765be9652d4
Tim Pechersky <tim.pechersky@gmail.com>
2020-08-27 08:27:02 +0300

    Updates

---
+++ precache-manifest.37fb01742d94c7ec2b76a33ccb1229c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56d1422cf736088dbf45796d2b1dd401",
+    "url": "/teknologic/index.html"
+  },
+  {
+    "revision": "58b9456354f3cf646179",
+    "url": "/teknologic/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9ee857d6a6d2d46b9065",
+    "url": "/teknologic/static/js/2.df3f80a6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/teknologic/static/js/2.df3f80a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58b9456354f3cf646179",
+    "url": "/teknologic/static/js/main.1a094c75.chunk.js"
+  },
+  {
+    "revision": "a8f88ed83373e7622a7a",
+    "url": "/teknologic/static/js/runtime-main.49419faf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/teknologic/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.78ee485fe12fd02619455b5a84a780f3.js"
+  "/teknologic/precache-manifest.37fb01742d94c7ec2b76a33ccb1229c6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/teknologic/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
