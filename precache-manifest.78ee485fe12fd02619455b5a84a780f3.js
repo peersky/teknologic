0d77b7ec7a79cd2647a0d8eb2022b72ea0e5a2bb
Tim Pechersky <tim.pechersky@gmail.com>
2020-08-27 08:10:58 +0300

    Updates

---
+++ precache-manifest.78ee485fe12fd02619455b5a84a780f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01b60b91bff26583b1f2ce38c62f7906",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6297ecd2abd788bdb6c1",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9ee857d6a6d2d46b9065",
+    "url": "/static/js/2.df3f80a6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.df3f80a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6297ecd2abd788bdb6c1",
+    "url": "/static/js/main.0e668c87.chunk.js"
+  },
+  {
+    "revision": "a38a68b8acbcd092f425",
+    "url": "/static/js/runtime-main.164e9cb8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
